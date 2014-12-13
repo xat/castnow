e937b6538de2cd1556c111153633551c5f7bb901
Tharshan <tharshan09@gmail.com>
2014-12-13 12:31:36 -0500

    fix indentation in format func

--- utils/unformat-time.js
+++ utils/unformat-time.js
@@ -3,17 +3,17 @@ module.exports = function (string) {
       seconds = 0;
   // turn hours and minutes into seconds and add them all up
   if (timeArray.length === 3) {
-      // hours
-      seconds = seconds + (parseInt(timeArray[0]) * 60 * 60);
-      // minutes
-      seconds = seconds + (parseInt(timeArray[1]) * 60);
-      // seconds
-      seconds = seconds + parseInt(timeArray[2]);
+    // hours
+    seconds = seconds + (parseInt(timeArray[0]) * 60 * 60);
+    // minutes
+    seconds = seconds + (parseInt(timeArray[1]) * 60);
+    // seconds
+    seconds = seconds + parseInt(timeArray[2]);
   } else if (timeArray.length === 2) {
-      // minutes
-      seconds = seconds + (parseInt(timeArray[0]) * 60);
-      // seconds
-      seconds = seconds + parseInt(timeArray[1]);
+    // minutes
+    seconds = seconds + (parseInt(timeArray[0]) * 60);
+    // seconds
+    seconds = seconds + parseInt(timeArray[1]);
   }
   return seconds;
 };
